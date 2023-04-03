54bb1477bdbd1b70b3818c5e7b4de44d3c264457
igor fagundes pontes <128741930+igorfagundes@users.noreply.github.com>
2023-04-03 13:34:31 -0300

    Update Principal.java
    finalizando codigos

--- ControleDeCirculacaoDeVeiculosLeves/controledeveiculosleves/Principal.java
+++ ControleDeCirculacaoDeVeiculosLeves/controledeveiculosleves/Principal.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Principal{
     static Date dataAtual = new Date();
     static Veiculo veiculo = new Veiculo();
-    static ArrayList<Veiculo> listaVeiculo = new ArrayList<>();
+    static ArrayList<Veiculo> listaVeiculo = new ArrayList<Veiculo>();
     public static void main(String[] args){
 
         int op = -1;
@@ -16,6 +16,16 @@ public class Principal{
                 marcarSaida();
             }else if(op == 2){
                 marcarEntrada();
+            }else if(op == 3){
+                imprimirTodosSaida();
+            }else if(op == 4){
+                imprimirTodosEntrada();
+            }else if(op == 5){
+                Scanner scan = new Scanner(System.in);
+                System.out.println("Digite a placa que deseja procurar");
+                System.out.print("Opcao: ");
+                String placa = scan.nextLine();
+                imprimirPorPlaca(placa);
             }else{
                 System.out.println("Obrigado por usar meu sistema");
                 System.out.println("fim do programa");
@@ -28,6 +38,9 @@ public class Principal{
         System.out.println("Escolha uma opcao ");
         System.out.println("[1] marcar saida");
         System.out.println("[2] marcar entrada");
+        System.out.println("[3] mostrar saidas");
+        System.out.println("[4] mostrar entradas");
+        System.out.println("[5] Procurar Placa");
         System.out.println("[0] sair");
         System.out.print("Opcao: ");
         return scan.nextInt();
@@ -35,10 +48,6 @@ public class Principal{
 //metodo para registrar saida
     public static void marcarSaida(){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Informe o horario de saida");
-        System.out.print("Opcao: ");
-        veiculo.setHoraDeSaida(scan.nextInt());
-        scan.nextLine();
         System.out.println("Informe a placa do veiculo");
         System.out.print("Opcao: ");
         veiculo.setPlaca(scan.nextLine());
@@ -52,14 +61,11 @@ public class Principal{
         System.out.print("Opcao: ");
         veiculo.setAtividade(scan.nextLine());
         listaVeiculo.add(veiculo);
+        imprimirSaida(veiculo);
     }
 //metodo para registrar entrada
     public static void marcarEntrada(){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Informe o horario de entrada");
-        System.out.print("Opcao: ");
-        veiculo.setHoraDeSaida(scan.nextInt());
-        scan.nextLine();
         System.out.println("Informe a placa do veiculo");
         System.out.print("Opcao: ");
         veiculo.setPlaca(scan.nextLine());
@@ -73,20 +79,49 @@ public class Principal{
         System.out.print("Opcao: ");
         veiculo.setAtividade(scan.nextLine());
         listaVeiculo.add(veiculo);
+        imprimirEntrada(veiculo);
     }
     public static ArrayList<Veiculo> imprimirSaida(Veiculo veic){
         System.out.println("Data e Hora de Saída" + dataAtual);
         System.out.println("Nome do Motorista: " + veic.getMotorista());
         System.out.println("Veículo: " + veic.getVeiculo());
         System.out.println("Placa: " + veic.getHoraDeSaida());
-        System.out.println("Ativiade: " + veic.getAtividade());
+        System.out.println("Atividade: " + veic.getAtividade());
         return listaVeiculo;
     }
-    public static void imprimirTodos(){
-
+    public static ArrayList<Veiculo> imprimirEntrada(Veiculo veic){
+        System.out.println("Data e Hora de Entrada" + dataAtual);
+        System.out.println("Nome do Motorista: " + veic.getMotorista());
+        System.out.println("Veículo: " + veic.getVeiculo());
+        System.out.println("Placa: " + veic.getHoraDeSaida());
+        System.out.println("Atividade: " + veic.getAtividade());
+        return listaVeiculo;
     }
-    public static ArrayList<Veiculo> imprimirPorPlaca(){
-
+    public static void imprimirTodosSaida(){
+        ArrayList<Veiculo> listaVeiculos = null;
+        for(int i = 0; i < listaVeiculos.size();i++){
+            listaVeiculo.get(i);
+            imprimirSaida(veiculo);
+        }
+    }
+    public static void imprimirTodosEntrada(){
+        ArrayList<Veiculo> listaVeiculos = null;
+        for(int i = 0; i < listaVeiculos.size();i++){
+            listaVeiculo.get(i);
+            imprimirEntrada(veiculo);
+        }
+    }
+    public static ArrayList<Veiculo> imprimirPorPlaca(String veic){
+        Veiculo veiculo = null;
+        for(int i = 0;i < listaVeiculo.size();i++){
+            veiculo = listaVeiculo.get(i);
+            if(listaVeiculo.get(i).equals(veic)){
+                imprimirTodosEntrada();
+                imprimirTodosSaida();
+            }else if(listaVeiculo.get(i).equals(veic) != listaVeiculo.get(i).equals(veic)){
+                System.out.println("Veiculo não encontrado!");
+            }
+        }
         return listaVeiculo;
     }
 }//global
